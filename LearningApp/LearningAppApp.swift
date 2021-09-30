49526bd7b6ebe4b0bdc9874c1ef5ca6d17b777fd
Alex Cannizzo <cansal89@gmail.com>
2021-09-30 21:46:38 +0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alex Cannizzo on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
