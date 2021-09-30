1321c3c7c88dafcc65f67c554e7087dcdba78ed8
Alex Cannizzo <cansal89@gmail.com>
2021-10-01 00:38:57 +0700

    - create lesson cards

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,13 +12,34 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.modules) { module in
+                            VStack(spacing: 20) {
+                                // learning card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                // test card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
